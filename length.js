a73325a305c226cb858baf4120cb00eb53bc97fd
ogujor1 <macfedo4real@gmail.com>
2024-10-03 05:03:01 +0100

    first commit

---
+++ length.js
@@ -0,0 +1,26 @@
+let arr = [1,2.5, 'food', true];
+console.log(arr.length);
+
+let arr2 = [1,2.5, 'food', true];
+console.log(arr.length -1);
+
+const arr1 = [
+  'femi',
+  2,
+  4.0,
+  false,
+  null,
+  undefined,
+  {name: 'eric'},
+  {'role': {position: 'keeper'}},
+];
+
+console.log(arr1[0]);
+console.log(arr1.length);
+console.log(arr1.length - 2);
+console.log(arr1[7]['role']['position']);
+
+let arr3 = [1,2,3,4,5,6,7,'okocha'];
+console.log(arr3[1]);
+console.log(arr3);
+console.log(arr3[arr3.length-1]);
---
+++ push.js
@@ -0,0 +1,9 @@
+// PUSH METHOD ADD ENTRIES TO THE END
+
+let ar = [1.0, 4, 5, 6, 7.5];
+// console.log(ar.push(67));
+console.log(ar);
+ar.push(67);
+console.log(ar);
+ar.push({version: "2.0"});
+console.log(ar);
